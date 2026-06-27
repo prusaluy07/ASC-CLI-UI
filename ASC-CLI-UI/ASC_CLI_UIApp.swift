86e5117a8ab21b9f56d29c2f8ac4faefb06de4fe
prusaluy07 <andre.ludwig@live.de>
2026-06-27 18:55:35 +0200

    Initial Commit

---
+++ ASC-CLI-UI/ASC_CLI_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ASC_CLI_UIApp.swift
+//  ASC-CLI-UI
+//
+//  Created by Andre Ludwig on 27.06.26.
+//
+
+import SwiftUI
+
+@main
+struct ASC_CLI_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
